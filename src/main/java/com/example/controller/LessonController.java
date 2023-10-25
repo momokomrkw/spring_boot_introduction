cc7995605895b5c60c92ffe8d0e1fbb354484800
Morikawa Momoko <mmk.muraoka@gmail.com>
2023-10-25 12:54:02 +0900

    Lesson01実装完了

--- src/main/java/com/example/controller/LessonController.java
+++ src/main/java/com/example/controller/LessonController.java
@@ -1,8 +1,8 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,8 +20,7 @@ public class LessonController {
 	}
 	
 	@GetMapping("/form_test")
-	public String formTest(SampleForm sampleForm, Model model) {
-		model.addAttribute("sampleForm", sampleForm);
+	public String formTest(@ModelAttribute SampleForm sampleForm) {
 		return "lesson/form_test";
 	}
 }
